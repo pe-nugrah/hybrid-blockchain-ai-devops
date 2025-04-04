b991f187afb53b73931e12153d45678b13c257c6
P. E. Nugrah <penugrah@criptext.com>
2025-04-04 19:02:36 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,12 @@
 # Instal Docker, Python, dan Git di Ubuntu
 sudo apt-get update
 sudo apt-get install -y docker.io python3-pip git
+
+# Aktifkan dan jalankan layanan Docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Tambahkan pengguna ke grup docker (opsional)
+# sudo usermod -aG docker $USER
+
+echo "Instalasi selesai! Jangan lupa logout/login atau reboot untuk apply group docker."
